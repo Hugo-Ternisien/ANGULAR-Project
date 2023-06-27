69d4207a24e84cc884e85b4a896508c70ea2d238
hugo.ternisien <ternisien.h@gmail.com>
2023-06-27 18:59:30 +0200

    feat: Carousel, main page and findbar implemented

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent implements OnInit {
+  items = [
+    { id: 1, title: 'Pizza', image: "assets/pizza.jpeg" },
+    { id: 2, title: 'Sushi', image: 'assets/sushi.jpeg' },
+    { id: 3, title: 'Burger', image: 'assets/burger.jpeg' },
+  ];
+
+  activeIndex = 0;
+  intervalId: any;
+  delay = 5000;
+
+  ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  startCarousel(): void {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, this.delay);
+  }
+
+  nextSlide(): void {
+    clearInterval(this.intervalId); // Clear the existing interval
+    this.activeIndex = (this.activeIndex + 1) % this.items.length;
+    this.delay = 5000;
+    this.startCarousel(); // Start a new interval
+  }
+
+  previousSlide(): void {
+    clearInterval(this.intervalId); // Clear the existing interval
+    this.activeIndex = (this.activeIndex - 1 + this.items.length) % this.items.length;
+    this.delay = 5000;
+    this.startCarousel(); // Start a new interval
+  }
+}
