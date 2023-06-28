70470373eb6a6df30ef26f636120a07adcbf9ae6
hugo.ternisien <ternisien.h@gmail.com>
2023-06-28 17:40:33 +0200

    feat: information div added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { InfoMainComponent } from './info-main/info-main.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    CarouselComponent
+    CarouselComponent,
+    InfoMainComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -10,6 +10,7 @@ export class CarouselComponent implements OnInit {
     { id: 1, title: 'Pizza', image: "assets/pizza.jpg" },
     { id: 2, title: 'Sushi', image: 'assets/sushi.jpg' },
     { id: 3, title: 'Burger', image: 'assets/burger.jpg' },
+    { id: 4, title: 'Ramen', image: 'assets/ramen.jpg' },
   ];
 
   activeIndex = 0;
@@ -36,5 +37,15 @@ export class CarouselComponent implements OnInit {
     clearInterval(this.intervalId); // Clear the existing interval
     this.activeIndex = (this.activeIndex - 1 + this.items.length) % this.items.length;
     this.startCarousel(); // Start a new interval
+  }
+
+  getVisibleItems(): any[] {
+    const visibleItems: any[] = [];
+    const prevIndex = (this.activeIndex - 1 + this.items.length) % this.items.length;
+    const nextIndex = (this.activeIndex + 1) % this.items.length;
+    visibleItems.push({ ...this.items[prevIndex], position: 'previous' });
+    visibleItems.push({ ...this.items[this.activeIndex], position: 'active' });
+    visibleItems.push({ ...this.items[nextIndex], position: 'next' });
+    return visibleItems;
   }
 }
