cf76b2dd878f1579e3d2a589c270665001f373f4
hugo.ternisien <ternisien.h@gmail.com>
2023-06-29 15:11:53 +0200

    feat: rating added

--- src/app/display-result-page/display-result-page.component.ts
+++ src/app/display-result-page/display-result-page.component.ts
@@ -35,4 +35,13 @@ export class DisplayResultPageComponent implements OnInit {
         }
       );
   }
+
+  getStarRating(rating: number): string {
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating - fullStars >= 0.5;
+    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+    return '★'.repeat(fullStars) + (hasHalfStar ? '½' : '') + '☆'.repeat(emptyStars);
+  }
+
 }
